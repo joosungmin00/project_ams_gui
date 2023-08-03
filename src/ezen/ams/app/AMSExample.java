0c24d60637c22b96b106187bdf5af0af1be8e46b
joosungmin00 <139952429+joosungmin00@users.noreply.github.com>
2023-08-03 09:27:39 +0900

    Update AMSExample.java

--- src/ezen/ams/app/AMSExample.java
+++ src/ezen/ams/app/AMSExample.java
@@ -6,7 +6,7 @@ import ezen.ams.exception.NotBalanceException;
 public class AMSExample {
 
 	public static void main(String[] args) {
-		Account account = new Account("김기정", 1111, 10000);
+		Account account = new Account("주성민", 1111, 10000);
 		try {
 			long restMoney = account.deposit(1000);//입력받는 값
 			System.out.println("정산처리 : " + account.getRestMoney());
